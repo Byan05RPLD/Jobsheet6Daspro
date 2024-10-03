1050aebb677184ddd67f89b45d0a2d6ff405b6a6
Anandya Almas <byanmalang123@gmail.com>
2024-10-03 20:03:08 +0700

    latihan 1

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -10,8 +10,6 @@ public class Pemilihan2Percobaan103 {
         if ((tahun % 4 ) == 0) {
             if((tahun % 100) !=0)
             System.out.println("Tahun Kabisat");
-            else
-            System.out.println("Bukan Tahun Kabisat");
         } else
         System.out.println("Bukan Tahun Kabisat");
     }
